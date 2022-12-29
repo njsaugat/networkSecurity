4773a4b2cb8b1f38a53e32a3e0b09ad7edbb0723
Saugat Poudel <njsaugat@gmail.com>
2022-12-29 16:27:13 +0545

    stegnography file added

--- stegnography.js
+++ stegnography.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
-const message = 'a';
+const message = 'A';
 const messageArray = message.charCodeAt(0).toString(2);
 let pointer = 0;
 // fs.readFile('d','utf-8',``)
+let bufferArray = [];
 fs.readFile('key.jpg', 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
@@ -11,17 +12,34 @@ fs.readFile('key.jpg', 'utf-8', (err, data) => {
   const buf = Buffer.from(data);
 
   //   console.log(data);
+
   let counter = 0;
   for (const item of buf) {
-    if (counter <= 100) {
-      let binaryString = item.toString(2);
+    let binaryString = item.toString(2);
+    let buffer;
+    if (counter >= 10000) {
+      console.log(item);
       const binaryStringArray = Array.from(binaryString);
       binaryStringArray[binaryStringArray.length - 1] = messageArray[pointer++];
       binaryString = binaryStringArray.join('');
-      console.log(binaryString);
-      //   console.log(bin)
+
+      binaryString = Number(binaryString);
+      binaryString = parseInt(binaryString, 2);
+      // console.log(binaryString);
+      buffer = Buffer.from(new Uint8Array(binaryString));
+    } else {
+      buffer = Buffer.from(new Uint8Array(item));
     }
+    bufferArray.push(buffer);
+    // console.log(buffer);
+    // fs.writeFileSync('key1.jpg', buffer, (err) => {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    // });
+    fs.writeFileSync('key1.jpg', buffer, { encoding: 'utf8', flag: 'w' });
     counter++;
     // console.log(item.toString())
   }
+  // console.log(bufferArray);
 });
